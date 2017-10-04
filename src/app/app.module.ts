963d60671a62715c86ff605396c0f6c62de5a3c4
Kobie Jones <kobiedomo@gmail.com>
2017-10-03 18:31:29 -0700

    Fixed the path problems removing /src/app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
-import { AuthComponent } from './src/app/auth/auth.component';
-import { LogInComponent } from './src/app/auth/log-in/log-in.component';
-import { SignupComponent } from './src/app/auth/signup/signup.component';
-import { FaqComponent } from './src/app/faq/faq.component';
-import { UserProfileComponent } from './src/app/user-profile/user-profile.component';
-import { PetComponent } from './src/app/pet/pet.component';
-import { PetScheduleComponent } from './src/app/pet/pet-schedule/pet-schedule.component';
-import { PetDetailComponent } from './src/app/pet/pet-detail/pet-detail.component';
-import { PetListComponent } from './src/app/pet/pet-list/pet-list.component';
+import { AuthComponent } from './auth/auth.component';
+import { LogInComponent } from './auth/log-in/log-in.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { FaqComponent } from './faq/faq.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { PetComponent } from './pet/pet.component';
+import { PetScheduleComponent } from './pet/pet-schedule/pet-schedule.component';
+import { PetDetailComponent } from './pet/pet-detail/pet-detail.component';
+import { PetListComponent } from './pet/pet-list/pet-list.component';
 
 @NgModule({
   declarations: [
