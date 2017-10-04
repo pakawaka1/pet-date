23d672458642fdeaff34ad0ea4d86061abcfc1bb
Kobie Jones <kobiedomo@gmail.com>
2017-10-03 18:23:01 -0700

    Added more generic components

---
+++ src/app/pet/pet-schedule/pet-schedule.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pet-schedule',
+  templateUrl: './pet-schedule.component.html',
+  styleUrls: ['./pet-schedule.component.css']
+})
+export class PetScheduleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
