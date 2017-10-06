995eca90031327e582e5ca29ae60b9f5e3033d7f
sfitzgerald16 <misfitz616@gmail.com>
2017-10-05 20:19:45 -0700

    styled login page

--- src/app/auth/log-in/log-in.component.ts
+++ src/app/auth/log-in/log-in.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-log-in',
@@ -6,8 +7,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./log-in.component.css']
 })
 export class LogInComponent implements OnInit {
+  form;
+  processing;
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder
+
+  ) {
+    this.createForm();
+   }
+
+   createForm() {
+     this.form = this.formBuilder.group({
+       email: ['', Validators.required],
+       password: ['', Validators.required],
+     })
+   }
+
+   disableForm() {
+     this.form.controls['email'].disable();
+     this.form.controls['password'].disable();
+   }
+
+   enableForm() {
+     this.form.controls['email'].enable();
+     this.form.controls['password'].enable();
+   }
+
+   onLoginSubmit() {
+    this.processing = true;
+    this.disableForm();
+    const user = {
+      email: this.form.get('email').value,
+      password: this.form.get('password').value
+    };
+    console.log(user);
+    this.processing = false;
+    this.form.reset()
+    this.enableForm();
+   }
+
 
   ngOnInit() {
   }
