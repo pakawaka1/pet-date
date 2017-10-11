49324a2c14eb7b15fb06670739831092c041f924
sfitzgerald16 <misfitz616@gmail.com>
2017-10-10 19:35:20 -0700

    added pet list

--- src/app/pet/pet-list/pet-list.component.ts
+++ src/app/pet/pet-list/pet-list.component.ts
@@ -7,6 +7,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PetListComponent implements OnInit {
 
+  allPets = [
+    {
+      "photo": "../assets/img/petphotos/sophie.jpg",
+      "name": "Sophie",
+      "type": "Dog",
+      "size": "Small",
+      "energy": "High",
+      "age": "2 yrs",
+      "rating": 4
+    },
+  {
+    "photo": "../assets/img/petphotos/pup6.jpg",
+    "name": "Sienna",
+    "type": "Dog",
+    "size": "Medium",
+    "energy": "Medium",
+    "age": "10 yrs",
+    "rating": 5
+  },
+  {
+    "photo": "../assets/img/petphotos/80.jpg",
+    "name": "Tinta",
+    "type": "Cat",
+    "size": "Medium",
+    "energy": "Medium",
+    "age": "4 yrs",
+    "rating": 4
+  },
+];
+
   constructor() { }
 
   ngOnInit() {
