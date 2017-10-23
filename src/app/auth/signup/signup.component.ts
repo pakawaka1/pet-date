c5b16f3abe975f95be5b0d3641c9bcbf82d9497b
Kobie Jones <kobiedomo@gmail.com>
2017-10-23 15:43:10 -0700

    Added some small changes to stuff, which I didn't want to push to master.

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-signup',
@@ -6,8 +8,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
+  form;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { 
+  	this.createForm();
+  }
+
+  createForm() {
+     this.form = this.formBuilder.group({
+       email: ['', Validators.required],
+       username: ['', Validators.required],
+       firstname: ['', Validators.required],
+       lastname: ['', Validators.required],
+       phonenumber: ['', Validators.required],
+       password: ['', Validators.required],
+       repassword: ['',Validators.required]
+     });
+   }
+
+   onLoginSubmit() {
+    const user = {
+      email: this.form.get('email').value,
+      username: this.form.get('username').value,
+      firstname: this.form.get('firstname').value,
+      lastname: this.form.get('lastname').value,
+      phonenumber: this.form.get('phonenumber').value,
+      password: this.form.get('password').value
+    };
+    console.log(user);
+   }
+
 
   ngOnInit() {
   }
