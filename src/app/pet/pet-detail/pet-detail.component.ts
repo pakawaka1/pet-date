79961050d0783578db488d2c2c91d52ca5189e42
David L Cervantes <pakawaka1@gmail.com>
2017-10-25 15:48:56 -0700

    pet-detail updated to database

--- src/app/pet/pet-detail/pet-detail.component.ts
+++ src/app/pet/pet-detail/pet-detail.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { PetsService } from '../../services/pets.service';
+import { AuthService } from '../../auth/auth.service';
+import { PetService } from '../pet.service';
+
 
 @Component({
   selector: 'app-pet-detail',
   templateUrl: './pet-detail.component.html',
   styleUrls: ['./pet-detail.component.css']
 })
+
 export class PetDetailComponent implements OnInit {
 
-  constructor() { }
+  loggedin = false;
 
-  ngOnInit() {
+
+  constructor(
+    private petsService: PetsService,
+    private petService: PetService,
+    private authService: AuthService
+  ) {
+
   }
 
+  ngOnInit () {
+     this.loggedin = this.authService.getCurrentUser()
+  }
 }
